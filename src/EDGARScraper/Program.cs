9e270af8a603ee3babe699eacb647c4d3f56da6b
Aaron Lovi <aaron.lovi@4poker.com>
2024-12-21 17:28:09 -0500

    Added Edgar scraper with test connection to the database

--- src/EDGARScraper/Program.cs
+++ src/EDGARScraper/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 
-namespace EDGARScraper;
-
-internal class Program
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Connect to MongoDB
+        var client = new MongoClient("mongodb://root:example@localhost:27017");
+        var database = client.GetDatabase("EDGAR");
+
+        // Test: List collections in the database
+        var collections = database.ListCollectionNames().ToList();
+        Console.WriteLine("Collections in the EDGAR database:");
+        collections.ForEach(Console.WriteLine);
     }
 }

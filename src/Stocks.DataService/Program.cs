a779619d8e8073438e9d99c40a3a946641993dce
Aaron Lovi <aaronlovi@gmail.com>
2025-01-11 16:42:16 -0500

    Implement gRPC service configuration and logging in Stocks.DataService

--- src/Stocks.DataService/Program.cs
+++ src/Stocks.DataService/Program.cs
@@ -1,16 +1,108 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Stocks.Shared;
+using Stocks.Persistence;
+
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Stocks.DataService;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        var builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddHostedService<Worker>();
+    private const string DefaultPortStr = "7101";
 
-        var host = builder.Build();
-        host.Run();
+    private static ILogger _logger;
+    private static IServiceProvider? _svp;
+
+    static Program()
+    {
+        _logger = HostingUtils.GetBootstrapLogger<Program>();
     }
+
+    private static int Main(string[] args)
+    {
+        try
+        {
+            _logger.LogInformation("Building the host");
+            var host = BuildHost<Startup>(args);
+            _logger.LogInformation("Running the host");
+            host.Run();
+
+            _logger.LogInformation("Service execution completed");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Service execution is terminated with an error");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    private static IHost BuildHost<TStartup>(string[] args) where TStartup : class
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<TStartup>(); })
+            .ConfigureServices((context, services) => {
+                var grpcPort = int.Parse(context.Configuration!.GetSection("Ports")["Grpc"] ?? DefaultPortStr, CultureInfo.InvariantCulture);
+                services.Configure<KestrelServerOptions>(opt =>
+                    {
+                        opt.ListenAnyIP(grpcPort, options => options.Protocols = HttpProtocols.Http2);
+                        opt.AllowAlternateSchemes = true;
+                    });
+
+                services
+                    .AddHttpClient()
+                    .AddSingleton<PostgresExecutor>()
+                    .AddSingleton<DbMigrations>();
+
+                if (DoesConfigContainConnectionString(context.Configuration))
+                    services.AddSingleton<IDbmService, DbmService>();
+
+                services.AddGrpc();
+            })
+            .ConfigureLogging((context, builder) => builder.ClearProviders())
+            .UseSerilog((context, LoggerConfiguration) =>
+                LoggerConfiguration.ReadFrom.Configuration(context.Configuration)
+            )
+            .Build();
+
+        _svp = host.Services;
+        _logger = _svp.GetRequiredService<ILogger<Program>>();
+        LogConfig(_svp.GetRequiredService<IConfiguration>());
+
+        return host;
+    }
+
+    private static bool DoesConfigContainConnectionString(IConfiguration configuration)
+        => configuration.GetConnectionString(DbmService.StocksDataConnectionStringName) is not null;
+
+    private static void LogConfig(IConfiguration _)
+    {
+        _logger.LogInformation("==========BEGIN CRITICAL CONFIGURATION==========");
+        //_logger.LogInformation("CompanyFactsBulkZipPath: {CompanyFactsBulkZipPath}", GetConfigValue("CompanyFactsBulkZipPath"));
+        //_logger.LogInformation("EdgarSubmissionsBulkZipPath: {EdgarSubmissionsBulkZipPath}", GetConfigValue("EdgarSubmissionsBulkZipPath"));
+        //LogConfigSection(config, GoogleCredentialsOptions.GoogleCredentials);
+        //LogConfigSection(config, HostedServicesOptions.HostedServices);
+        //LogConfigSection(config, FeatureFlagsOptions.FeatureFlags);
+        _logger.LogInformation("==========END CRITICAL CONFIGURATION==========");
+    }
+
+    //private static string GetConfigValue(string key)
+    //{
+    //    if (_svp is null)
+    //        throw new InvalidOperationException("Service provider is not initialized");
+    //    IConfiguration configuration = _svp.GetRequiredService<IConfiguration>();
+    //    return configuration[key] ?? throw new InvalidOperationException($"Configuration key '{key}' not found");
+    //}
 }
---
+++ src/Stocks.DataService/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stocks.DataService;
+
+/// <summary>
+/// 1. Application Initialization
+/// When the application starts, the Startup class is instantiated and configured.
+/// </summary>
+internal class Startup(IConfiguration _config)
+{
+    /// <summary>
+    /// 2. Service Configuration
+    /// The ConfigureServices method is called by the runtime.
+    /// This method is used to add services to the DI container.
+    /// These services are then available throughout the application
+    /// </summary>
+    /// <param name="services"></param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddGrpc();
+
+        VerifyCriticalConfiguration();
+    }
+
+    /// <summary>
+    /// 3. Request Pipeline Configuration
+    /// The Configure method is called by the runtime to set up the request processing pipeline.
+    /// This method defines how the application will respond to HTTP requests.
+    /// </summary>
+    public static void Configure(IApplicationBuilder app)
+    {
+        app.
+            UseRouting().
+            UseEndpoints(endpoints =>
+            {
+                // In this case, incoming HTTP requests are routed to gRPC endpoints,
+                // which are configured in ReportingHostConfig.ConfigureEndpoints
+                var builders = new List<GrpcServiceEndpointConventionBuilder>();
+                builders.AddRange(ReportingHostConfig.ConfigureEndpoints(endpoints));
+            });
+    }
+
+    #region PRIVATE HELPER METHODS
+
+    private void VerifyCriticalConfiguration()
+    {
+        VerifyConfigurationItem("DatabaseSchema");
+    }
+
+    private void VerifyConfigurationItem(string key, string? section = null)
+    {
+        string? value;
+        if (section is not null)
+        {
+            value = _config.GetSection(section)[key];
+            if (string.IsNullOrEmpty(value))
+                throw new Exception($"Missing '{key}' in '{section}' section of app configuration");
+        }
+        else
+        {
+            value = _config[key];
+            if (string.IsNullOrEmpty(value))
+                throw new Exception($"Missing '{key}' in app configuration");
+        }
+    }
+
+    #endregion
+}

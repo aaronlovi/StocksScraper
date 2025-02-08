904b2b2dc356800fbbb0ddff0ab94c81c1332407
Aaron Lovi <aaronlovi@gmail.com>
2025-02-08 10:07:08 -0500

    Add Docker Compose configuration for topz service and update PostgresExecutor comments

--- src/Stocks.Persistence/PostgresExecutor.cs
+++ src/Stocks.Persistence/PostgresExecutor.cs
@@ -217,7 +217,7 @@ public class PostgresExecutor : IDisposable
 
     public void Dispose()
     {
-        // Do not change this code. Pub cleanup code in 'Dispose(bool disposing)' method
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
